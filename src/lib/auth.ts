db4bef01c8670125ce8034848e4f38b47f9d6aa1
DanielAssuncaoDeveloper <danielassuncao.developer@gmail.com>
2025-08-09 13:56:57 -0300

    :wrench: chore: Configurando BetterAuth.

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -3,6 +3,9 @@ import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { betterAuth } from "better-auth";
 
 export const auth = betterAuth({
+  emailAndPassword: {
+    enabled: true,
+  },
   database: drizzleAdapter(db, {
     provider: "pg",
   }),
