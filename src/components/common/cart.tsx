c32f5749bd31813126949f725b87244f9ed66171
DanielAssuncaoDeveloper <danielassuncao.developer@gmail.com>
2025-08-12 22:16:32 -0300

    :sparkles: feat: Implementando ação para adicionar produtos ao carrinho e esquema de validação.

--- src/components/common/cart.tsx
+++ src/components/common/cart.tsx
@@ -1,3 +1,5 @@
+"use client";
+
 import { ShoppingBasketIcon } from "lucide-react";
 import { Sheet, SheetContent, SheetTrigger } from "../ui/sheet";
 import { Button } from "../ui/button";
@@ -11,7 +13,7 @@ const Cart = () => {
         </Button>
       </SheetTrigger>
       <SheetContent>
-        
+
       </SheetContent>
     </Sheet>
     );
